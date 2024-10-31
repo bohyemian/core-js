07624134387ff4971b2950d30f85cba9b3cb0cc1
bohyemian <tomboy.kr@gmail.com>
2024-10-31 10:16:05 +0900

    수정

--- client/chapter/core/02.variable.js
+++ client/chapter/core/02.variable.js
@@ -2,34 +2,57 @@
 /* Variables          */
 /* ------------------ */
 
+console.log('variable'); //호이스팅 undefined 초기화
+
 let admin;
 let name;
+
 name = 'John';
 admin = name;
+
 console.log(admin);
 
-let ourPlanetName = 'Earth';
-let currentUserName = 'John';
+const OUR_PLANET_NAME = 'EARTH';
+let currentOnlineUserName = 'TIGER';
+
+currentOnlineUserName = 'beom';
 
 /* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
 
-// - 갯수 별 상품 가격 계산하기
-let countedPrice;
-// - 구매 제품 가격의 총 합
-let totalPrice;
-// - 1년 기준 일(day)자 수
-const DAY_PER_YEAR = 365;
-// - 구매 결제 여부
-let isPayed;
-// - 구매 결제 내역
-let payedList;
-// - 브랜드 접두사
-const BRAND_PREFIX = '';
-// - 오늘의 운세
-let todayFortune;
-// - 상품 정보
-const productInformation = {};
-
 /* variables ----------------------------------------------------------- */
 
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 구매 결제 여부
+let isPayment = true;
+let hasCartItem = false;
+
+// - 구매 결제 내역
+let paymentList;
+
+// const product = {
+//   productA:{
+//     name:'',
+//     price:'',
+//     info:''
+//   }
+// }
+
+// - 오늘의 운세
+let fortuneToday = 'so good';
+
 /* constant variables -------------------------------------------------- */
+
+// - 1년 기준 일(day)자 수
+
+const DAY_PER_YEAR = 365;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Stussy';
+
+// - 상품 정보
+const product_info = '....';
