a44ad2e7fafd7610423ac8c0dc7116621d9a9389
bohyemian <tomboy.kr@gmail.com>
2024-11-15 12:32:52 +0900

    주석 삭제

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -1,5 +1,3 @@
-/* global typeError */
-
 function getAttr(node, prop) {
   if (isString(node)) node = getNode(node);
   if (!isString(prop)) throw typeError('getAttr 함수에 전달된 두 번째 인수는 문자 타입 이어야 합니다.');
