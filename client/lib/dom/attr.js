81c233b565d0f412b37d6c69cc9ca48f9884a048
bohyemian <tomboy.kr@gmail.com>
2024-11-19 19:18:56 +0900

    module

--- client/lib/dom/attr.js
+++ client/lib/dom/attr.js
@@ -1,35 +1,68 @@
-function getAttr(node, prop) {
-  if (isString(node)) node = getNode(node);
-  if (!isString(prop)) throw typeError('getAttr 함수에 전달된 두 번째 인수는 문자 타입 이어야 합니다.');
+import { isString, isUndefined } from '../utils/type.js';
+import { getNode } from "./getNode.js";
+import { typeError } from "../error/typeError.js";
 
-  return node.getAttribute(prop);
+function getAttr(node,prop){
+
+  if(isString(node)) node = getNode(node);
+
+  if(!isString(prop)) throw typeError('getAttr 함수에 전달된 두 번째 인수는 문자 타입 이어야 합니다.')
+
+
+  return node.getAttribute(prop)
+
 }
 
-function setAttr(node, prop, value) {
-  if (isString(node)) node = getNode(node);
-  if (!isString(prop)) throw typeError('setAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
-  if (isNull(value) || value === '') {
+
+function setAttr(node,prop,value){
+  
+  if(isString(node)) node = getNode(node);
+  if(!isString(prop)) throw typeError('setAttr 함수의 두 번째 인수는 문자 타입 이어야 합니다.');
+  if(isUndefined(value) || value === ''){
+    
     node.removeAttribute(prop);
     return;
   }
 
-  if (prop.startsWith('data')) {
+  if( prop.startsWith('data') ) {
     prop = prop.slice(5);
     node.dataset[prop] = value;
     return;
   }
 
-  node.setAttribute(prop, value);
+  node.setAttribute(prop,value);
+
 }
 
-// function attr(node, prop, value){
-//   if (isUndefined(value)) {
-//     return getAttr(node, prop)
-//   } else {
-//     setAttr(node, prop, value)
+
+// function attr(node,prop,value){
+  
+//   if(!value){
+//     return getAttr(node,prop)
+//   }else{
+//     setAttr(node,prop,value)
 //   }
 // }
 
-const attr = (node, prop, value) => (isUndefined(value) ? getAttr(node, prop) : setAttr(node, prop, value));
+export  const attr = (node,prop,value) => !value ? getAttr(node,prop) : setAttr(node,prop,value)
+
 // attr('.about','id') // getter
-// attr('.first', 'id', 'star'); // setter
+// attr('.about','id','star') // setter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- client/main.js
+++ client/main.js
@@ -1,5 +1,31 @@
-const a = 'hello';
+// named export           =>  import { } from '..'
+// default export         =>  import ... from '..'
 
-function sum() {
-  console.log(1 + 2);
+// import { getNode as $, getNodes } from './lib/dom/getNode.js';
+// import { insertLast } from './lib/dom/insert.js';
+
+// import clearContents from "./lib/dom/clearContents.js";
+
+import { getNode as $, getNodes, typeError, insertLast, clearContents } from './lib/index.js';
+
+// 1. input 선택하기
+// 2. input 이벤트 바인딩
+// 3. input의 value 값 가져오기
+// 4. 숫자 더하기
+// 5. result에 출력하기
+
+const first = $('#firstNumber');
+const second = $('#secondNumber');
+const result = $('.result');
+
+function handleInput() {
+  const firstValue = Number(first.value);
+  const secondValue = +second.value;
+  const total = firstValue + secondValue;
+
+  clearContents(result);
+  insertLast(result, total);
 }
+
+first.addEventListener('input', handleInput);
+second.addEventListener('input', handleInput);
