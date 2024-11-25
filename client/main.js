0e8d516f555b4e726119fbc460dd79ad27768865
bohyemian <tomboy.kr@gmail.com>
2024-11-25 13:24:48 +0900

    json-server 추가

--- client/main.js
+++ client/main.js
@@ -1,17 +1,18 @@
-import { bono, changeColor, delayP, getNode, renderEmptyCard, renderSpinner, renderUserCard } from './lib/index.js';
+import { bono, changeColor, clearContents, delayP, getNode, getNodes, renderEmptyCard, renderSpinner, renderUserCard } from './lib/index.js';
 
 // const response = await fetch('https://jsonplaceholder.typicode.com/users');
 // 👆 전역에서 await를 사용하는 경우. Top-level await :: https://fe-developers.kakaoent.com/2022/220728-es2022/
 // 마치 Top-level await를 사용한 모듈이 하나의 거대한 async 함수처럼 동작하게 됩니다.
 
-const END_POINT = 'https://jsonplaceholder.typicode.com/users';
-
+// const END_POINT = 'https://jsonplaceholder.typicode.com/users';
+const END_POINT = 'http://localhost:3000/users';
 const userCardInner = getNode('.user-card-inner');
-
-renderSpinner(userCardInner);
+const createButton = getNode('.create');
+const cancelButton = getNode('.cancel');
+const doneButton = getNode('.done');
 
 async function renderUserList() {
-  await delayP(500);
+  renderSpinner(userCardInner);
 
   try {
     gsap.to('.loadingSpinner', {
@@ -31,14 +32,12 @@ async function renderUserList() {
       x: -50,
       y: 50,
       opacity: 0,
-      stagger: 0.1,
+      stagger: { each: 0.1, from: 'start' },
     });
   } catch {
     renderEmptyCard(userCardInner);
   }
 }
-
-renderUserList();
 
 function handleDeleteCard(e) {
   const button = e.target.closest('button');
@@ -46,9 +45,40 @@ function handleDeleteCard(e) {
   if (!button) return;
 
   const index = button.parentElement.dataset.index.slice(5);
+
   bono.delete(`${END_POINT}/${index}`).then(() => {
-    alert('삭제가 완료됐습니다.');
+    clearContents(userCardInner);
+    renderUserList();
   });
 }
 
+function handleCreate() {
+  // this.classList.add('open');
+  gsap.to('.pop', { autoAlpha: 1 });
+}
+
+function handleCancel(e) {
+  e.stopPropagation();
+  // createButton.classList.remove('open');
+  gsap.to('.pop', { autoAlpha: 0 });
+}
+
+function handlePost() {
+  const inputList = getNodes('input');
+  const [name, email, website] = [...inputList].map((input) => input.value);
+
+  if (name !== '' && email !== '' && website !== '') {
+    bono.post(END_POINT, { username: name, email, website }).then(() => {
+      renderUserList();
+      clearContents(userCardInner);
+      gsap.to('.pop', { autoAlpha: 0 });
+    });
+  }
+}
+
+renderUserList();
+
 userCardInner.addEventListener('click', handleDeleteCard);
+createButton.addEventListener('click', handleCreate);
+cancelButton.addEventListener('click', handleCancel);
+doneButton.addEventListener('click', handlePost);
