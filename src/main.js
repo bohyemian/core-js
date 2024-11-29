6c5ad98265c2cf691571436a8c2ce21f0d864304
bohyemian <tomboy.kr@gmail.com>
2024-11-29 10:25:22 +0900

    web component에서 style 사용하기

--- src/main.js
+++ src/main.js
@@ -1,16 +1,1 @@
-import '@/style/style.css';
-import { btn } from '@/style/style.module.css';
-import { getNode, insertLast } from 'kind-tiger';
-import gorani from '@/assets/gorani.png';
-
-const app = getNode('#app');
-
-const template = /*html*/ `
-  <figure class="conitaner">
-    <img src="${gorani}" alt="" style="width:30vw" />
-    <figcaption>상냥 고라니</figcaption>
-  </figure>
-  <button type="button" class=${btn}>button</button>
-`;
-
-insertLast(app, template);
+import {} from '/src/Layout/Header/Header';
