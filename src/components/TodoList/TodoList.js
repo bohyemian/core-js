4fb611538fe3248d961c4ec92d6d261fc4647049
bohyemian <tomboy.kr@gmail.com>
2024-12-02 18:12:43 +0900

    TO DO id, title 추가

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -11,32 +11,34 @@ class TodoList extends LitElement {
 
   constructor() {
     super();
-    this.todoItems = JSON.parse(localStorage.getItem('todo')) || [];
+    this.todoTitle = this.getAttribute('data-todo-title') || '';
+    this.todoId = this.getAttribute('data-todo-id') || '';
+    this.todoItems = JSON.parse(localStorage.getItem('todo' + this.todoId)) || [];
   }
 
   handleAddItem() {
     const newItem = { id: Date.now(), value: '', checked: false };
     this.todoItems = [...this.todoItems, newItem];
-    this.saveData();
+    this.saveData(this.todoId);
   }
 
   handleDelete(id) {
     this.todoItems = this.todoItems.filter((item) => item.id !== id);
-    this.saveData();
+    this.saveData(this.todoId);
   }
 
   handleUpdate(id, updateItem) {
     this.todoItems = this.todoItems.map((item) => (item.id === id ? { ...item, ...updateItem } : item)); //item -> {value, checked}
-    this.saveData();
+    this.saveData(this.todoId);
   }
 
-  saveData() {
-    localStorage.setItem('todo', JSON.stringify(this.todoItems));
+  saveData(todoId) {
+    localStorage.setItem('todo' + todoId, JSON.stringify(this.todoItems));
   }
   render() {
     return html`
       <div class="container">
-        <h1>TO DO LIST</h1>
+        <h1>TO DO LIST${this.todoTitle ? ` (${this.todoTitle})` : null}</h1>
         <ul class="todo">
           ${this.todoItems.map((item) => html/* html */ ` <todo-item .id=${item.id} .value=${item.value} .checked=${item.checked} @update=${(e) => this.handleUpdate(item.id, e.detail)} @delete=${() => this.handleDelete(item.id)}></todo-item> `)}
         </ul>
